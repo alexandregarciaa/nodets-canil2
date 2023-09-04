4a640993c5fca45c6b5d636bb5725e06025e11db
Alexandre Garcia <alexandre.garciaa@hotmail.com>
2023-09-04 16:40:16 -0300

    final project

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
-import mainRoutes from './routes';
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -10,14 +10,14 @@ const server = express();
 
 server.set('view engine','mustache');
 server.set('views', path.join(__dirname, 'views'));
-server.set('mustache', mustache());
+server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
 
 server.use(mainRoutes);
 
 server.use((req, res)=> {    
-    res.send('Página não encontrada');
+    res.render('pages/404');
 });
 
-server.listen(process.env.PORT);    
+server.listen(process.env.PORT);     
